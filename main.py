77a1bfd2a184818e44d8c5822e97d455f3f594ca
mdinnus <mdinnus@uni-muenster.de>
2025-07-25 10:15:16 +0200

    Implemented full LLM and Azure API pipeline

--- main.py
+++ main.py
@@ -2,27 +2,27 @@
 
 object = "windows" # This is the object the user wants to find in the image range
 
-for i in range(4593, 4653):
-    # # Reasoning operation
-    # data_reasoning = {
-    #     "DATASET_NUMBER": str(i),
-    #     "VOCAB_SECONDLVL": object
-    # }
-    # try:
-    #     with open(f"reasoning.py") as file:
-    #         exec(file.read(), data_reasoning)
-    # except RuntimeError as e:
-    #     print(e)
-    #     continue
+for i in range(4607, 4608):
+    # Reasoning operation
+    data_reasoning = {
+        "DATASET_NUMBER": str(i),
+        "VOCAB_SECONDLVL": object
+    }
+    try:
+        with open(f"reasoning.py") as file:
+            exec(file.read(), data_reasoning)
+    except RuntimeError as e:
+        print(e)
+        continue
 
-    # # Subprocesses for flat and nested operations
-    # data_subprocesses = {
-    #     "DATASET_NUMBER": str(i),
-    #     "SCALE_UP_SIZE": 512,
-    #     "VOCAB_GROUNDTRUTH": data_reasoning['VOCAB_GROUNDTRUTH'],
-    #     "VOCAB_FRSTLVL": data_reasoning['VOCAB_FRSTLVL'],
-    #     "VOCAB_SECONDLVL": object
-    # }
+    # Subprocesses for flat and nested operations
+    data_subprocesses = {
+        "DATASET_NUMBER": str(i),
+        "SCALE_UP_SIZE": 512,
+        "VOCAB_GROUNDTRUTH": data_reasoning['VOCAB_GROUNDTRUTH'],
+        "VOCAB_FRSTLVL": data_reasoning['VOCAB_FRSTLVL'],
+        "VOCAB_SECONDLVL": object
+    }
     data_subprocesses = {
         "DATASET_NUMBER": str(i),
         "SCALE_UP_SIZE": 512,
